30379a22a2097b9e1231d4c3d98e23ac8a9065fa
onemidzuk <kuzneca@gmail.com>
2020-01-05 11:36:12 +0300

    edit

--- backup_ver2.py
+++ backup_ver2.py
@@ -1,4 +1,4 @@
-# import os
+import os
 import time
 
 source = ['/home/midzuk/PycharmProjects/', '/home/midzuk/Документы']
@@ -11,7 +11,7 @@ now = time.strftime('%H%M%S')
 
 if not os.path.exists(today):
     os.mkdir(today)
-print('Catalogue succesfully created!')
+print('Directory is succesfully created!')
 
 #target = target_dir + os.sep + time.strftime('%Y%m%d%H%M%S') + '.zip'
 
@@ -23,11 +23,11 @@ else:
     target = today + os.sep + now + '_' + \
              comment.replace(' ', '_') + '.zip'
 
-tarfile.open(name=None, mode='r', fileobj=None, bufsize=10240, **kwargs)
+#tarfile.open(name=None, mode='r', fileobj=None, bufsize=10240, **kwargs)
 zip_command = "zip -qr {0} {1}".format(target, ' '.join(source))
 
-
-if os.system(zip_command) == 0:
-    print('Backup is succesfully competed. Your copy is in', target)
-else:
-    print('Backup failed!')
+print(zip_command)
+# if os.system(zip_command) == 0:
+#     print('Backup is succesfully competed. Your copy is in', target)
+# else:
+#     print('Backup failed!')
